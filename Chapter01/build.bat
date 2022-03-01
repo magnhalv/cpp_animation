e01abc7a3d19d655b304a9627f0a9eec87890c19
Magnus Halvorsen <magnus.halvorsen@acando.no>
2022-03-01 18:03:15 +0100

    Add basic openGL window example

---
+++ Chapter01/build.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set CommonCompilerFlags=-MTd -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4505 -wd4100 -wd4189 -DGAME_INTERNAL=1 -DGAME_SLOW=1 -FC -Z7 /std:c++latest /EHsc
+set CommonLinkerFlags= -incremental:no -opt:ref user32.lib Gdi32.lib winmm.lib opengl32.lib
+
+IF NOT EXIST build mkdir build
+pushd build
+
+cl %CommonCompilerFlags%  ../src/WinMain.cpp ../src/glad.c /link %CommonLinkerFlags%
+popd
